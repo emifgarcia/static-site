6793b27a2217fe517442d4b87d8959b6bac32812
Marcelo Figueiredo Garcia <marcelo.garcia@pq.itv.org>
2025-03-25 20:57:53 -0300

    static-site

---
+++ script.js
@@ -0,0 +1,4 @@
+// Script simples para alterar o texto quando o botão é clicado
+document.getElementById("changeTextButton").addEventListener("click", function() {
+    document.getElementById("dynamicText").textContent = "O texto foi alterado com sucesso!";
+});
